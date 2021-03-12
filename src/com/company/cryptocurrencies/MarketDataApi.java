29fdaa258f83f7666c50a566f64ce4bc6aa0e8e8
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-03-12 10:26:20 +0100

    added connection with cryptocurrency api required to create chart

--- src/com/company/cryptocurrencies/MarketDataApi.java
+++ src/com/company/cryptocurrencies/MarketDataApi.java
@@ -1,6 +1,8 @@
 package com.company.cryptocurrencies;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,25 +11,34 @@ import java.net.http.HttpResponse;
 
 public class MarketDataApi {
 
-    private static JSONObject dataObject;
+    private static JSONObject apiDataObject;
     private static Cryptocurrency cryptocurrency;
+    private static Chart chart;
 
-    public void getData(Cryptocurrency cryptocurrency)
+    public MarketDataApi(Cryptocurrency cryptocurrency, Chart chart)
     {
-        this.cryptocurrency = cryptocurrency;
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+cryptocurrency.getCurrentCryptocurrency()+"&tsyms="+cryptocurrency.getCurrentCurrency())).build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(MarketDataApi::parse)
-                .join();
+        this.cryptocurrency=cryptocurrency;
+        this.chart = chart;
+
     }
 
-    private static void parse(String responseData)
+    public void getData()
     {
-        dataObject= new JSONObject(responseData);
-        dataObject= dataObject.getJSONObject("RAW").getJSONObject(cryptocurrency.getCurrentCryptocurrency()).getJSONObject(cryptocurrency.getCurrentCurrency());
-        setData(dataObject);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + cryptocurrency.getCurrentCryptocurrency() + "&tsyms=" + cryptocurrency.getCurrentCurrency())).build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(MarketDataApi::parseData)
+                    .join();
+    }
+
+    private static void parseData(String responseData)
+    {
+
+            apiDataObject = new JSONObject(responseData);
+            apiDataObject = apiDataObject.getJSONObject("RAW").getJSONObject(cryptocurrency.getCurrentCryptocurrency()).getJSONObject(cryptocurrency.getCurrentCurrency());
+            setData(apiDataObject);
 
     }
 
@@ -36,6 +47,49 @@ public class MarketDataApi {
         cryptocurrency.setPrice(dataObject.getFloat("PRICE"));
         cryptocurrency.setChange24H(dataObject.getFloat("CHANGE24HOUR"));
         cryptocurrency.setAveragePrice((dataObject.getFloat("HIGHDAY")+dataObject.getFloat("LOWDAY"))/2);
+    }
+
+    public void getHourlyChartData()
+    {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://min-api.cryptocompare.com/data/v2/histohour?fsym="+cryptocurrency.getCurrentCryptocurrency()+"&tsym="+ cryptocurrency.getCurrentCurrency()+"&limit=10")).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(MarketDataApi::parseChartData)
+                .join();
+    }
+
+    public void getDailyChartData()
+    {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://min-api.cryptocompare.com/data/v2/histoday?fsym="+cryptocurrency.getCurrentCryptocurrency()+"&tsym="+ cryptocurrency.getCurrentCurrency()+"&limit=10")).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(MarketDataApi::parseChartData)
+                .join();
+    }
+
+    private static void parseChartData(String responseData)
+    {
+        JSONObject data = new JSONObject(responseData);
+        JSONArray array = data.getJSONObject("Data").getJSONArray("Data");
+        setChartData(array);
+
+    }
+
+    private static void setChartData(JSONArray array)
+    {
+        JSONObject tmp;
+        for(int i=2;i<array.length();i++)
+        {
+            tmp=array.getJSONObject(i);
+            chart.addPrice(tmp.getFloat("open"));
+            chart.addTimestamp(tmp.getInt("time"));
+        }
+
+
     }
 
 
---
+++ src/com/company/main/MainFrameListener.java
@@ -0,0 +1,213 @@
+package com.company.main;
+
+import com.company.cryptocurrencies.Cryptocurrency;
+import com.company.cryptocurrencies.MarketDataApi;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainFrameListener extends MainFrame implements ActionListener {
+
+   private Cryptocurrency cryptocurrency = new Cryptocurrency();
+   private MarketDataApi api;
+   private Timer timer;
+
+   private boolean dailyChartDataRange = true;
+
+
+    public MainFrameListener()
+    {
+
+        api= new MarketDataApi(cryptocurrency,chart);
+
+        bitcoinItem.addActionListener(this);
+        ethereumItem.addActionListener(this);
+        cardanoItem.addActionListener(this);
+        xrpItem.addActionListener(this);
+        litecoinItem.addActionListener(this);
+        polkadotItem.addActionListener(this);
+        uniswapProtocolItem.addActionListener(this);
+
+        usdItem.addActionListener(this);
+        plnItem.addActionListener(this);
+        eurItem.addActionListener(this);
+        chfItem.addActionListener(this);
+
+
+        twoSeconds.addActionListener(this);
+        tenSeconds.addActionListener(this);
+        thirtySeconds.addActionListener(this);
+        oneMinute.addActionListener(this);
+
+       addButtonsActionListener();
+
+        setData();
+          getChartData();
+          createChart();
+        updateTimer();
+
+
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+        if(actionEvent.getSource()==bitcoinItem)
+        {  cryptocurrency.setCurrentCryptocurrency("BTC");
+
+        }
+        if(actionEvent.getSource()==ethereumItem) {
+            cryptocurrency.setCurrentCryptocurrency("ETH");
+        }
+        if(actionEvent.getSource()==xrpItem) {
+            cryptocurrency.setCurrentCryptocurrency("XRP");
+        }
+        if(actionEvent.getSource()==cardanoItem) {
+            cryptocurrency.setCurrentCryptocurrency("ADA");
+        }
+        if(actionEvent.getSource()==litecoinItem) {
+            cryptocurrency.setCurrentCryptocurrency("LTC");
+        }
+        if(actionEvent.getSource()==polkadotItem) {
+            cryptocurrency.setCurrentCryptocurrency("DOT");
+        }
+        if(actionEvent.getSource()==uniswapProtocolItem) {
+            cryptocurrency.setCurrentCryptocurrency("UNI");
+        }
+
+        if(actionEvent.getSource()==usdItem) {
+            cryptocurrency.setCurrentCurrency("USD");
+        }
+        if(actionEvent.getSource()==plnItem) {
+            cryptocurrency.setCurrentCurrency("PLN");
+        }
+        if(actionEvent.getSource()==chfItem) {
+            cryptocurrency.setCurrentCurrency("CHF");
+        }
+        if(actionEvent.getSource()==eurItem) {
+            cryptocurrency.setCurrentCurrency("EUR");
+        }
+
+        if(actionEvent.getSource()==twoSeconds){
+            updateTime=2000;
+            timer.cancel();
+            updateTimer();
+        }
+        if(actionEvent.getSource()==tenSeconds){
+            updateTime=1000;
+            timer.cancel();
+            updateTimer();
+        }
+        if(actionEvent.getSource()==thirtySeconds){
+            updateTime=30000;
+            timer.cancel();
+            updateTimer();
+        }
+        if(actionEvent.getSource()==oneMinute){
+            updateTime=60000;
+            timer.cancel();
+            updateTimer();
+        }
+
+        setData();
+        erasePreviousChartData();
+        getChartData();
+        createChart();
+
+    }
+
+
+    private void setData()
+    {
+        api.getData();
+        currentPriceLabel.setText(cryptocurrency.getPrice() +" "+ cryptocurrency.getCurrentCurrency());
+        averagePriceLabel.setText(cryptocurrency.getAveragePrice() +" "+ cryptocurrency.getCurrentCurrency());
+
+        if(cryptocurrency.getChange24H()<0)
+        {
+            change24HValueLabel.setForeground(Color.red);
+        }
+        else
+        {
+            change24HValueLabel.setForeground(Color.green);
+        }
+        change24HValueLabel.setText(cryptocurrency.getChange24H() +" "+ cryptocurrency.getCurrentCurrency());
+
+        cryptoType.setText(cryptocurrency.getCurrentCryptocurrency().toString());
+
+    }
+
+    private void updateTimer()
+    {
+        timer = new Timer();
+        timer.schedule(  new TimerTask()
+        {
+            @Override
+            public void run() {
+                setData();
+            }
+        },0,updateTime);
+    }
+
+
+
+    private void addButtonsActionListener()
+    {
+
+        dailyDataButton.addActionListener(actionEvent -> {
+            dailyChartDataRange=true;
+            erasePreviousChartData();
+            getChartData();
+            createChart();
+        });
+
+        hourlyDataButton.addActionListener(actionEvent -> {
+            dailyChartDataRange=false;
+        erasePreviousChartData();
+        getChartData();
+        createChart();
+        });
+    }
+
+
+    private void erasePreviousChartData()
+    {
+        chart.clearArray();
+    }
+
+    private void getChartData()
+    {
+        if(dailyChartDataRange)
+        {
+            api.getDailyChartData();
+        }
+
+        else
+        {
+            api.getHourlyChartData();
+        }
+    }
+
+    private void createChart()
+    {
+
+        if(dailyChartDataRange)
+        {
+            chart.setDailyChartDataRange(true);
+        }
+        else
+        {
+            chart.setDailyChartDataRange(false);
+        }
+        chart.createChart(cryptocurrency.getCurrentCurrency());
+    }
+
+
+
+}
